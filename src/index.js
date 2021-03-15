532feeaf3878c044ce202743f420f1139fa64e9d
Feeniixx <s19240@pjwstk.edu.pl>
2021-03-15 23:13:17 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (!array || !array.length) return 0;
+  else {
+      var result = Math.min(...array);
+      return result;
+  }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array || !array.length) return 0;
+  else {
+      var result = Math.max(...array);
+      return result;
+  }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (!array || !array.length) return 0;
+  else {
+      let result = array.reduce((a, b) => a + b) / array.length;
+      return result;
+  }
+};
